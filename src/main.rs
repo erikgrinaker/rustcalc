e6ad889c9d74986a4abf90192181b7c5dcc32ede
Erik Grinaker <erik@grinaker.org>
2019-03-03 19:31:06 +0100

    Simple REPL that echoes the input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![warn(clippy::all)]
+
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut line = String::new();
+
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let result = line.trim();
+        println!("{}", result);
+    }
 }
