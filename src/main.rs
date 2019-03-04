f29ec36603e1ac4cdf1d5eb60f8bda7c8009b9f7
Erik Grinaker <erik@grinaker.org>
2019-03-04 21:22:51 +0100

    Separated out prompt() function

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,23 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    loop {
-        let mut line = String::new();
-
-        print!("> ");
-        io::stdout().flush().unwrap();
-        match io::stdin().read_line(&mut line) {
-            Ok(n) => {
-                if n == 0 {
-                    println!();
-                    break;
-                }
-                let result = line.trim();
-                if !result.is_empty() {
-                    println!("{}", result);
-                }
-            }
-            Err(error) => {
-                panic!(error);
-            }
+    while let Some(expr) = prompt(">") {
+        if expr.is_empty() {
+            continue;
         }
+        println!("{}", expr)
+    }
+    println!(); // Newline after ^D
+}
+
+fn prompt(prompt: &str) -> Option<String> {
+    print!("{} ", prompt);
+    io::stdout().flush().unwrap();
+
+    let mut line = String::new();
+    if io::stdin().read_line(&mut line).unwrap() > 0 {
+        Some(line.trim().to_string())
+    } else {
+        None
     }
 }
