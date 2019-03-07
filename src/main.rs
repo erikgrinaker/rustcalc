416fbfad2bcd98697d863cc733a9ea8955374725
Erik Grinaker <erik@grinaker.org>
2019-03-07 22:51:24 +0100

    Added CLI struct

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,55 @@ mod error;
 mod lexer;
 mod parser;
 
-use rustyline::Editor;
 use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 use error::Error;
 use parser::Parser;
 
 fn main() -> Result<(), Error> {
-    let mut rl = Editor::<()>::new();
-    while let Some(input) = prompt(&mut rl, "> ")? {
-        match evaluate(&input) {
-            Ok(Some(n)) => println!("{}", n),
-            Err(e) => println!("Error: {}", e),
-            Ok(None) => continue,
+    CLI::new().run()
+}
+
+struct CLI {
+    prompt: Editor<()>,
+}
+
+impl CLI {
+    fn new() -> CLI {
+        CLI {
+            prompt: Editor::<()>::new(),
         }
     }
-    println!(); // Newline after ^D
-    Ok(())
-}
 
-fn evaluate(input: &str) -> Result<Option<f64>, Error> {
-    if !input.is_empty() {
-        Parser::new().parse(input).map(|expr| Some(expr.evaluate()))
-    } else {
-        Ok(None)
+    fn evaluate(&mut self, input: &str) -> Result<Option<f64>, Error> {
+        if !input.is_empty() {
+            Parser::new().parse(input).map(|expr| Some(expr.evaluate()))
+        } else {
+            Ok(None)
+        }
     }
-}
 
-fn prompt(rl: &mut Editor<()>, prompt: &str) -> Result<Option<String>, Error> {
-    match rl.readline(prompt) {
-        Ok(input) => {
-            rl.add_history_entry(input.as_ref());
-            Ok(Some(input))
-        },
-        Err(ReadlineError::Eof) => Ok(None),
-        Err(ReadlineError::Interrupted) => Ok(None),
-        Err(err) => Err(Error::IOError(format!("{}", err))),
+    fn prompt(&mut self) -> Result<Option<String>, Error> {
+        match self.prompt.readline("> ") {
+            Ok(input) => {
+                self.prompt.add_history_entry(input.as_ref());
+                Ok(Some(input))
+            }
+            Err(ReadlineError::Eof) => Ok(None),
+            Err(ReadlineError::Interrupted) => Ok(None),
+            Err(err) => Err(Error::IOError(format!("{}", err))),
+        }
+    }
+
+    fn run(&mut self) -> Result<(), Error> {
+        while let Some(input) = self.prompt()? {
+            match self.evaluate(&input) {
+                Ok(Some(n)) => println!("{}", n),
+                Err(e) => println!("Error: {}", e),
+                Ok(None) => continue,
+            }
+        }
+        Ok(())
     }
 }
