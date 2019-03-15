13399469de5fa52a28c06e55f02ef7192e860b2b
Erik Grinaker <erik@grinaker.org>
2019-03-15 22:59:47 +0100

    Added a bunch of tests

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -1,27 +1,60 @@
 extern crate rustcalc;
 
 use rustcalc::parser::Parser;
+use std::f64;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn eval(input: &str, result: f64) {
-        assert_eq!(result, Parser::new(input).parse().unwrap().evaluate())
+macro_rules! test_eval {
+    ( $( $name:ident: $value:expr, )* ) => {
+    $(
+        #[test]
+        fn $name() {
+            let (input, expect) = $value;
+            assert_eq!(expect, Parser::new(input).parse().unwrap().evaluate())
+        }
+    )*
     }
+}
 
-    #[test]
-    fn test_precedence() {
-        eval("3 * 2 + 1", 7.0)
-    }
+test_eval! {
+    // Literals
+    number:             ("1",           1.0),
+    number_decimal:     ("3.14",        3.14),
 
-    #[test]
-    fn test_infix() {
-        eval("3 + 2 - 1", 4.0)
-    }
+    // Prefix operators
+    assert:             ("+1",          1.0),
+    assert_multi:       ("+++1",        1.0),
 
-    #[test]
-    fn test_prefix() {
-        eval("-- +-3.14", -3.14)
-    }
+    negate:             ("-1",          -1.0),
+    negate_assert:      ("-+-+-1",      -1.0),
+    negate_multi:       ("---1",        -1.0),
+
+    // Postfix operators
+    factorial:          ("5!",          120.0),
+    factorial_multi:    ("3!!",         720.0),
+    factorial_zero:     ("0!",          1.0),
+
+    // Infix operators
+    add:                ("1 + 2",       3.0),
+    add_negative:       ("1 + -2",      -1.0),
+
+    divide:             ("6 / 2",       3.0),
+    divide_negative:    ("6 / -2",      -3.0),
+    divide_zero:        ("1 / 0",       f64::INFINITY),
+
+    exp:                ("2 ^ 3",       8.0),
+    exp_fraction:       ("8 ^ 1/3",     2.0),
+    exp_negative:       ("2 ^ -3",      0.125),
+    exp_zero:           ("2 ^ 0",       1.0),
+    exp_zero_zero:      ("0 ^ 0",       1.0),
+
+    modulo:             ("5 % 2",       1.0),
+    modulo_divisible:   ("4 % 2",       0.0),
+    modulo_negative:    ("-5 % 2",      -1.0),
+    modulo_negative2:   ("5 % -2",      1.0),
+
+    multiply:           ("2 * 3",       6.0),
+    multiply_zero:      ("2 * 3",       6.0),
+
+    subtract:           ("1 - 2",       -1.0),
+    subtract_negative:  ("1 - -2",      3.0),
 }
