57ede616a17bf04be5650065a94bbb39bd4f7f8e
Erik Grinaker <erik@grinaker.org>
2019-03-17 21:48:40 +0100

    Tweaked lexer code

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,20 +4,34 @@ use std::str::Chars;
 
 use crate::error::Error;
 
-#[derive(Debug, PartialEq)]
+// A lexer token
+#[derive(Clone, Debug, PartialEq)]
 pub enum Token {
+    /// A literal number
     Number(String),
+    /// A name, of a constant or function
     Ident(String),
+    /// The addition symbol +
     Plus,
+    /// The subtraction symbol -
     Minus,
+    /// The multiplication symbol *
     Asterisk,
+    /// The division symbol /
     Slash,
+    /// The exponentiation symbol ^
     Caret,
+    /// The square root symbol √
     SquareRoot,
+    /// The modulo symbol %
     Percent,
+    /// The factorial symbol !
     Exclamation,
+    /// An opening parenthesis
     OpenParen,
+    /// A closing parenthesis
     CloseParen,
+    /// An expression separator ,
     Comma,
 }
 
@@ -41,6 +55,7 @@ impl fmt::Display for Token {
     }
 }
 
+/// A lexer tokenizes an input string as an iterator
 pub struct Lexer<'a> {
     iter: Peekable<Chars<'a>>,
 }
@@ -58,16 +73,50 @@ impl<'a> Iterator for Lexer<'a> {
 }
 
 impl<'a> Lexer<'a> {
+    /// Creates a new lexer for the given input string
     pub fn new(input: &'a str) -> Lexer<'a> {
         Lexer {
             iter: input.chars().peekable(),
         }
     }
 
+    /// Consumes any whitespace characters
     fn consume_whitespace(&mut self) {
         self.next_while(|c| c.is_whitespace());
     }
 
+    /// Grabs the next character if it matches the predicate function
+    fn next_if<F>(&mut self, predicate: F) -> Option<char>
+    where
+        F: Fn(char) -> bool,
+    {
+        self.iter.peek().filter(|&c| predicate(*c))?;
+        self.iter.next()
+    }
+
+    /// Grabs the next single-character token if the tokenizer function returns one
+    fn next_if_token<F>(&mut self, tokenizer: F) -> Option<Token>
+    where
+        F: Fn(char) -> Option<Token>,
+    {
+        let token = self.iter.peek().and_then(|&c| tokenizer(c))?;
+        self.iter.next();
+        Some(token)
+    }
+
+    /// Grabs the next characters that match the predicate, as a string
+    fn next_while<F>(&mut self, predicate: F) -> Option<String>
+    where
+        F: Fn(char) -> bool,
+    {
+        let mut value = String::new();
+        while let Some(c) = self.next_if(&predicate) {
+            value.push(c)
+        }
+        Some(value).filter(|v| !v.is_empty())
+    }
+
+    /// Scans the input for the next token if any, ignoring leading whitespace
     fn scan(&mut self) -> Option<Token> {
         self.consume_whitespace();
         None.or_else(|| self.scan_ident())
@@ -76,34 +125,37 @@ impl<'a> Lexer<'a> {
             .or_else(|| self.scan_punctuation())
     }
 
+    /// Scans the input for the next ident token, if any
     fn scan_ident(&mut self) -> Option<Token> {
         let mut name = self.next_if(|c| c.is_alphabetic())?.to_string();
-        if let Some(rest) = self.next_while(|c| c.is_alphanumeric() || c == '_') {
-            name.push_str(rest.as_str())
+        while let Some(c) = self.next_if(|c| c.is_alphanumeric() || c == '_') {
+            name.push(c)
         }
         Some(Token::Ident(name))
     }
 
+    /// Scans the input for the next number token, if any
     fn scan_number(&mut self) -> Option<Token> {
         let mut num = self.next_while(|c| c.is_digit(10))?;
         if let Some(sep) = self.next_if(|c| c == '.') {
             num.push(sep);
-            if let Some(dec) = self.next_while(|c| c.is_digit(10)) {
-                num.push_str(&dec)
+            while let Some(dec) = self.next_if(|c| c.is_digit(10)) {
+                num.push(dec)
             }
         }
         if let Some(exp) = self.next_if(|c| c == 'e' || c == 'E') {
             num.push(exp);
-            if let Some(sign) = self.next_if(|c| c == '-' || c == '+') {
+            if let Some(sign) = self.next_if(|c| c == '+' || c == '-') {
                 num.push(sign)
             }
-            if let Some(n) = self.next_while(|c| c.is_digit(10)) {
-                num.push_str(&n)
+            while let Some(c) = self.next_if(|c| c.is_digit(10)) {
+                num.push(c)
             }
         }
         Some(Token::Number(num))
     }
 
+    /// Scans the input for the next operator token, if any
     fn scan_operator(&mut self) -> Option<Token> {
         self.next_if_token(|c| match c {
             '+' => Some(Token::Plus),
@@ -118,6 +170,7 @@ impl<'a> Lexer<'a> {
         })
     }
 
+    /// Scans the input for the next punctuation token, if any
     fn scan_punctuation(&mut self) -> Option<Token> {
         self.next_if_token(|c| match c {
             '(' => Some(Token::OpenParen),
@@ -125,36 +178,5 @@ impl<'a> Lexer<'a> {
             ',' => Some(Token::Comma),
             _ => None,
         })
-    }
-
-    fn next_if<F>(&mut self, predicate: F) -> Option<char>
-    where
-        F: Fn(char) -> bool,
-    {
-        self.iter
-            .peek()
-            .cloned()
-            .filter(|&c| predicate(c))
-            .and_then(|_| self.iter.next())
-    }
-
-    fn next_if_token<F>(&mut self, predicate: F) -> Option<Token>
-    where
-        F: Fn(char) -> Option<Token>,
-    {
-        let token = self.iter.peek().and_then(|&c| predicate(c))?;
-        self.iter.next();
-        Some(token)
-    }
-
-    fn next_while<F>(&mut self, predicate: F) -> Option<String>
-    where
-        F: Fn(char) -> bool,
-    {
-        let mut value = String::new();
-        while let Some(c) = self.next_if(&predicate) {
-            value.push(c)
-        }
-        Some(value).filter(|v| !v.is_empty())
     }
 }
